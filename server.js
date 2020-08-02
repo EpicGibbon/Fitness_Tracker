7649a68a23915ddba777f142f1c4d8e8cfc88538
EpicGibbon <michant1997@gmail.com>
2020-08-02 19:40:08 -0400

    added additional folder structure. should start seeding next

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 const express = require("express");
 const path = require("path");
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 const session = require("express-session");
+
+//set up PORT
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 //importing our routes
 const routes = require("./routes")
-//set up PORT
-const PORT = process.env.PORT || 3000;
 
 //use middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// set up express-session
-app.use(session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true
-}));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness_tracker", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+
 
 // use routes
-app.use(routes);
+app.use(require("./routes/apiRoutes"))
+app.use(require("./routes/htmlRoutes"))
+
+
 
 app.listen(PORT, () => {
     console.log(`app listening on: http://localhost:${PORT}`);
